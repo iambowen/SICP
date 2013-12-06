1076118e4fff36be2360b4f8d59d89a480567ef5
tcz001 <fan.torchz@gmail.com>
2013-12-06 22:26:44 +0800

    1.3 fixed for sum of two largest num in three

--- chapter1/1.3.scm
+++ chapter1/1.3.scm
@@ -3,5 +3,22 @@
   )
 
 (define (max-of-three a b c)
-  (max (max a b) (max b c))
-)
+  (max c (max a b))
+  )
+
+(define (max-of-three? a b c x)
+  (= (max-of-three a b c) x)
+  )
+
+(define (second-largest-of-three a b c)
+  (cond ((max-of-three? a b c a) (max b c))
+        ((max-of-three? a b c b) (max a c))
+        ((max-of-three? a b c c) (max a b))
+        )
+  )
+
+(define (sum-of-max-two-of-three a b c)
+  (+ (max-of-three a b c) (second-largest-of-three a b c))
+  )
+
+(sum-of-max-two-of-three 5 3 6)
